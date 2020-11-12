9db0e0ea5553249b94f076ee6e408e788d2b0dc6
tang_huibo <1215893264@qq.com>
2020-11-12 23:28:20 +0800

    basic init

---
+++ swagger-aggregate-web/src/test/java/io/github/tanghuibo/swaggeraggregateweb/SwaggerAggregateWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.swaggeraggregateweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerAggregateWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
